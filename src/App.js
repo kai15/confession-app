4078fd8e7d7d5b6bb3cd090323987b5f7a9a5958
kakakai <kaisakai15@gmail.com>
2023-08-06 03:10:41 +0700

    #main: version 1.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logoG from './g.gif';
+import logoSmile from './smile-png.gif';
 import './App.css';
 import {useState} from "react";
+import useSound from 'use-sound';
+import bgMusic from "./bg-music-end.mp3";
 
 function App() {
   const [opening, setOpening] = useState(true)
@@ -8,10 +11,14 @@ function App() {
   const [index, setIndex] = useState(0)
   const [data, setData] = useState({title: "", desc: ""})
 
+  const [play] = useSound(bgMusic, {volume: 0.1});
+
   const handleOpening = () => {
+    play();
     setOpening(false)
     setContent(true)
-    handleContent(index + 1)
+    // handleContent(index + 1)
+    handleContent()
   }
 
   const handleContent = (indexx) => {
@@ -47,14 +54,14 @@ function App() {
           desc: "Udah dari 2021 kenal, tapi baru ketemu 2023 mana ga jelas lagi akunya, tapi masih sabar. hehehe"
         })
         break
-      // case 6:
-      //   setData({
-      //     title: "Vape",
-      //     desc: "Si sosoan ga ngevape pas ketemu, padahal mah kita santuy ajeeeee."
-      //   })
+      case 6:
+        setData({
+          title: "Ngambek",
+          desc: "Meski sabar tapi tetep aja namanya cewek ya ges ya, ngambek mulu hermannnnnn"
+        })
         break
       default:
-        setData({title: "", desc: ""})
+        setData({title: "Dear Gadis terhebat ku,", desc: ""})
         break
     }
   }
@@ -75,13 +82,51 @@ function App() {
           <header className="App-content">
             {/*<img src={logoG} className="App-logo" alt="logo" />*/}
             <div style={{backgroundColor: "darkslategrey", padding: "0 20px", width: "80%", height: 50, margin: "30px 0"}}>
-              <h1 style={{textAlign: "left", fontWeight: "normal"}}>{data?.title}</h1>
-              <h1 style={{textAlign: "left", fontWeight: "normal"}}></h1>
+              <h2 style={{textAlign: "left", fontWeight: "normal"}}>{data?.title}</h2>
             </div>
-            <p style={{marginTop: 15, textAlign: "left", }}>
-              {data?.desc}
+            <p style={{marginTop: 25, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              mungkin, <br />
+              kata sabar ga akan mengikis kecewa mu, <br />
+              kata semangat ga akan menghapus lelah mu, <br />
             </p>
-            <div style={{display: "flex", marginLeft: "auto", marginRight: 0}}>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              namun, <br />
+              kata sayang semoga bisa menenangkan mu, <br />
+              satu pelukan semoga bisa menghangatkan mu, <br />
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              meski, <br />
+              beberapa pihak dengan sengaja menyakiti mu, <br />
+              bahkan terlalu jauh mencampuri pilihan hidup mu, <br />
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              tetap, <br />
+              terima kasih sudah, masih dan selalu bertahan, <br />
+              jangan menyerah ya cantik ku, <br />
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              hari ini, <br />
+              kemarin besok bahkan lusa, <br />
+              semoga bahagia senantiasa menyertai kita, <br />
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              sayang, <br />
+              aku geli baca tulisan ku sendiri, <br />
+              hehehe, i love you :) <br />
+            </p>
+
+            <div style={{display: "none", marginLeft: "auto", marginRight: 0}}>
               {index > 1 &&
                 <div className="App-link" style={{marginRight: "auto", marginLeft: 0}}  onClick={() => handleContent(index - 1)}>
                   {"<<"}
@@ -91,6 +136,7 @@ function App() {
                 >>
               </div>
             </div>
+            <img src={logoSmile} className="App-logo" alt="logo" />
           </header>}
     </div>
   );

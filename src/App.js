53a9daa5730c521d061d175691dfc23a24368ff1
kakakai <kaisakai15@gmail.com>
2023-09-29 02:25:48 +0700

    #main: version 1.6

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import logoSmile from './smile-png.gif';
 import './App.css';
 import {useState} from "react";
 import useSound from 'use-sound';
-import bgMusic from "./bg-music-end.mp3";
+// import bgMusic from "./bg-music-end.mp3";
+import bgMusicAmin from "./bg-music-amin.mp3";
 
 function App() {
   const [opening, setOpening] = useState(true)
   const [content, setContent] = useState(false)
+  const [contentSixMonth, setContentSixMonth] = useState(false)
   const [index, setIndex] = useState(0)
   const [data, setData] = useState({title: "", desc: ""})
 
-  const [play] = useSound(bgMusic, {volume: 0.5});
+  // const [play] = useSound(bgMusic, {volume: 0.5});
+  const [playAmin] = useSound(bgMusicAmin, {volume: 0.5});
 
   const handleOpening = () => {
-    play();
+    playAmin();
     setOpening(false)
-    setContent(true)
+    setContentSixMonth(true)
     // handleContent(index + 1)
+    console.log("index", index, data)
     handleContent()
   }
 
@@ -61,7 +65,7 @@ function App() {
         })
         break
       default:
-        setData({title: "Dear Gadis terhebat ku,", desc: ""})
+        setData({title: "Dear Gadis kesayangan ku,", desc: ""})
         break
     }
   }
@@ -74,15 +78,89 @@ function App() {
             <p>
               C O N F E S S I O N
             </p>
-            <div className="App-link" onClick={() => handleOpening()}>
+            <div className="App-link" style={{cursor: "pointer"}} onClick={() => handleOpening()}>
               START
             </div>
           </header>}
-        {content &&
+        {contentSixMonth &&
           <header className="App-content">
             {/*<img src={logoG} className="App-logo" alt="logo" />*/}
             <div style={{backgroundColor: "darkslategrey", padding: "0 20px", width: "80%", height: 50, margin: "30px 0"}}>
-              <h2 style={{textAlign: "left", fontWeight: "normal"}}>{data?.title}</h2>
+              <h2 style={{textAlign: "left", fontWeight: "normal"}}>Dear Gadis kesayangan ku,</h2>
+            </div>
+            <p style={{marginTop: 25, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              Halo sayang, r u ready? <br />
+              Agaknya membaca tulisanku akan menjadi salah satu rutinitas baru mu, ya jika kamu mau tentu. hehe <br/>
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              Bulan ini diawali dengan hal yg selama ini kamu hindari, <br/>
+              bukan dengan kondisi yg diinginkan sebenarnya, <br/>
+              tentu bukan langkah yg mudah, <br/>
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              tapi kamu akhirnya memilih untuk berani, <br/>
+              berani menghadapi dengan ketetapan hati, <br/>
+              apapun jadinya rasamu setelah itu, <br/>
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              aku tetap di sini, <br/>
+              menunggumu, <br/>
+              bersamamu, <br/>
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              dikala kamu kembali dengan senyuman, <br/>
+              jelas lega dan bangga, <br/>
+              sayang… kamu hebat, <br/>
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              terima kasih kamu, <br/>
+              sudah berusaha dan percaya, <br/>
+              dengan dirimu sendiri dan aku, <br/>
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              genggam erat jangan lepas, <br/>
+              ayo terus bersama, <br/>
+              selama waktu masih ada, <br/>
+            </p>
+
+            <p style={{marginTop: 15, textAlign: "left", }}>
+              {/*{data?.desc}*/}
+              untuk kita, <br/><br/>
+
+              Selamat bulan ke 6 sayang, <br/>
+              I love you :) <br/>
+            </p>
+
+            <div style={{marginLeft: "auto", marginRight: 0}}>
+              <div className="App-link" style={{marginLeft: "auto", marginRight: 0, cursor: "pointer"}}  onClick={() => {
+                setContentSixMonth(false);
+                setContent(true);
+                // play();
+              }}>
+                >>
+              </div>
+            </div>
+            <img src={logoSmile} className="App-logo" alt="logo" />
+          </header>}
+
+      {content &&
+          <header className="App-content">
+            {/*<img src={logoG} className="App-logo" alt="logo" />*/}
+            <div style={{backgroundColor: "darkslategrey", padding: "0 20px", width: "80%", height: 50, margin: "30px 0"}}>
+              <h2 style={{textAlign: "left", fontWeight: "normal"}}>{"Dear gadis terhebat ku,"}</h2>
             </div>
             <p style={{marginTop: 25, textAlign: "left", }}>
               {/*{data?.desc}*/}
@@ -126,15 +204,22 @@ function App() {
               hehehe, i love you :) <br />
             </p>
 
-            <div style={{display: "none", marginLeft: "auto", marginRight: 0}}>
-              {index > 1 &&
-                <div className="App-link" style={{marginRight: "auto", marginLeft: 0}}  onClick={() => handleContent(index - 1)}>
-                  {"<<"}
-                </div>}
-              {index > 1 && <div style={{width: "50%", color: "#136649"}}>space</div>}
-              <div className="App-link" style={{marginLeft: "auto", marginRight: 0}}  onClick={() => handleContent(index + 1)}>
-                >>
+            <div style={{marginLeft: "auto", marginRight: 0}}>
+              <div className="App-link" style={{marginRight: "auto", marginLeft: 0}}  onClick={() => {
+                setContent(false);
+                setContentSixMonth(true);
+                playAmin();
+              }}>
+                {"<<"}
               </div>
+              {/*{index > 1 &&*/}
+              {/*    <div className="App-link" style={{marginRight: "auto", marginLeft: 0}}  onClick={() => handleContent(index - 1)}>*/}
+              {/*      {"<<"}*/}
+              {/*    </div>}*/}
+              {/*{index > 1 && <div style={{width: "50%", color: "#136649"}}>space</div>}*/}
+              {/*<div className="App-link" style={{marginLeft: "auto", marginRight: 0, cursor: "pointer"}}  onClick={() => handleContent(index + 1)}>*/}
+              {/*  >>*/}
+              {/*</div>*/}
             </div>
             <img src={logoSmile} className="App-logo" alt="logo" />
           </header>}
